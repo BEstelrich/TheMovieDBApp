aa66a007361e3791d0a3d230cbc4c5202339c5d6
Bartolome Estelrich <b.estelrich@mycloudportion.com>
2019-12-14 15:04:25 -0500

    User interface | Details view
    Vary for traits added in order to accommodate landscape mode on the iPhones.

--- TheMovieDBApp/Views/Subviews/MovieCell.swift
+++ TheMovieDBApp/Views/Subviews/MovieCell.swift
@@ -37,3 +37,61 @@ class MovieCellBackgroundView: UIView {
 }
 
 
+@IBDesignable
+class MoviePosterView: UIImageView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        defaultLayout()
+    }
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        defaultLayout()
+    }
+    
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        defaultLayout()
+    }
+    
+    
+    func defaultLayout() {
+        self.applyRoundedCourners(of: Constants.Design.roundedCornersRadius)
+    }
+
+    
+}
+
+
+@IBDesignable
+class PlayTrailerButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        defaultLayout()
+    }
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        defaultLayout()
+    }
+    
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        defaultLayout()
+    }
+    
+    
+    func defaultLayout() {
+        self.applyRoundedCourners(of: 12)
+    }
+
+    
+}
+
+
